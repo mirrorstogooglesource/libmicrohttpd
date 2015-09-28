e8ccdec728c8528c1bc1a7e3ba265a762d259018
Alex Vakulenko <avakulenko@google.com>
2015-09-28 16:28:19 -0700

    Remove GCM cipher mode requirements for fallback ciphersuites
    SecureTransport of MacOS/iOS does not support GCM cipher mode with
    ECDHE-ECDSA/ECDHE-RSA key-exchange algorithms even though they seem
    to claim that they do. This is supposedly fixed in MacOS 10.11 but
    that one is not out yet.
    
    BUG: 24468826
    Change-Id: Ie62a9b11584b6745848090888b7d9e1fe50e90ee

--- src/microhttpd/daemon.c
+++ src/microhttpd/daemon.c
@@ -706,8 +706,8 @@ MHD_TLS_init (struct MHD_Daemon *daemon)
       ret = SSL_CTX_set_cipher_list (daemon->tls_context,
                                      "ECDHE-ECDSA-AES128-GCM-SHA256:"
                                      "ECDHE-ECDSA-AES256-GCM-SHA384:"
-                                     "ECDHE-RSA-AES128-GCM-SHA256:"
-                                     "ECDHE-RSA-AES256-GCM-SHA384");
+                                     "ECDHE-RSA-AES128-SHA256:"
+                                     "ECDHE-RSA-AES256-SHA384");
       if (ret == 0)
          {
 #if HAVE_MESSAGES
